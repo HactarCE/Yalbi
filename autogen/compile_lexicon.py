32c26670351006c8b58655ac076be617d4397ecf
Hactar <legomany3448@gmail.com>
2017-08-11 16:39:29 -0400

    ✨ ROOTS

--- autogen/compile_lexicon.py
+++ autogen/compile_lexicon.py
@@ -4,6 +4,7 @@
 # This script requires the HJSON package and titlecase
 # You can install them both with a quick and easy `pip install hjson titlecase`
 
+from collections import OrderedDict
 from os import path
 import re
 import sys
@@ -27,6 +28,7 @@ except ImportError:
 scriptname = path.basename(sys.argv[0])
 
 files = '''\
+roots_template.md
 modifiers_template.md
 '''
 
@@ -35,49 +37,88 @@ def md_linkify(header_text):
 
 def format_roots(source):
     # [TODO] Warn if CV value is the same as another root or might conflict with a modifier
-    return '[TODO]\n'
+    with open(source) as f:
+        contents = ''
+        real_output = '\n'
+        for category_name, roots in hjson.loads(f.read()).items():
+            contents += '* [{}](#{})\n'.format(titlecase(category_name), md_linkify(category_name))
+            real_output += '## {}\n\n'.format(titlecase(category_name))
+            for root, data in roots.items():
+                if root.startswith('@'):
+                    continue
+                try:
+                    definition = data['d'] if isinstance(data['d'], str) else data['d'][0]
+                    if isinstance(data['s'], OrderedDict):
+                        stems = ''
+                        for stem, meaning in data['s'].items():
+                            if not stem.startswith('_'):
+                                stems += '\n**{}** - {}  '.format(stem, meaning)
+                        if stems:
+                            stems = stems[1:-2] + '\n'
+                    else:
+                        if data['s'] == root:
+                            stems = ''
+                            for stem, meaning in roots['@' + root]['s'].items():
+                                if not stem.startswith('_'):
+                                    if 'a@' in meaning and (not isinstance(data['d'], str) or len(data['d'] < 2)):
+                                        print("WARNING: no dictionary article ('a' or 'an') for '{}' in '{}' roots list".format(root, category_name))
+                                        raise Exception('continue')
+                                    stems += '\n**{}** - {}  '.format(stem, meaning[0].replace('a@', data['d'][1] + '@').replace('@', definition))
+                            if stems:
+                                stems = stems[1:-2] + '\n'
+                        else:
+                            try:
+                                d = roots[data['s']]['d']
+                                link_to_pattern = md_linkify('-{}- {}'.format(data['s'][0][1:], d if isinstance(d, str) else d[0]))
+                                stems = 'This root is patterned after the root [-{}-](#{}).\n'.format(data['s'].upper(), link_to_pattern)
+                            except KeyError:
+                                print("WARNING: unable to find pattern for '{}' in '{}' roots list".format(root, category_name))
+                                continue
+                    output = "##### -{}- '{}'\n\n{}\n".format(root.upper(), definition.upper(), stems)
+                    if 'src' in data:
+                        output += "This root is based on -{}- from [Ithkuil 2011](http://www.ithkuil.net/lexicon.htm).\n\n".format(data['src'].upper())
+                    if data['s'] == root and 'n' in roots['@' + root]:
+                        output += roots['@' + root]['n'] + '\n\n'
+                    if 'n' in data:
+                        output += data['n'] + '\n\n'
+                    contents += "  * [-{}- '{}'](#{})\n".format(root.upper(), definition.upper(), md_linkify('-{}- {}'.format(root, definition)))
+                    real_output += output
+                except KeyError as e:
+                    print("WARNING: missing `{}` value on '{}' in '{}' roots list".format(e.args[0], root, category_name))
+                except Exception as e:
+                    if not e.args[0] == 'continue':
+                        raise
+    return contents + real_output[:-1]
 
 def format_modifiers(source):
     with open(source) as f:
         contents = ''
         real_output = '\n'
-        def handle_key_error(*args):
-            print("WARNING: missing `{}` value on '{}' in '{}' modifiers list".format(*args))
         for category_name, modifiers in hjson.loads(f.read()).items():
             contents += '* [{}](#{})\n'.format(titlecase(category_name), md_linkify(category_name))
-            real_output += '## {0}\n\n'.format(titlecase(category_name))
+            real_output += '## {}\n\n'.format(titlecase(category_name))
             for abbreviation, data in modifiers.items():
                 try:
-                    try:
-                        output = '### `{}` {}\n\n'.format(abbreviation.upper(), titlecase(data['g']))
-                    except KeyError:
-                        handle_key_error('g', abbreviation, category_name)
-                        raise
-                    try:
-                        # [TODO] Warn if consonantal value is the same as a case modifier
-                        # [TODO] Warn if consonantal value conflicts with another modifier
-                        output += 'Cm value: **{}**  \n'.format(data['c'])
-                    except KeyError:
-                        handle_key_error('c', abbreviation, category_name)
-                        raise
+                    output = '### `{}` {}\n\n'.format(abbreviation.upper(), titlecase(data['g']))
+                    # [TODO] Warn if consonantal value is the same as a case modifier
+                    # [TODO] Warn if consonantal value conflicts with another modifier
+                    output += 'Cm value: **{}**  \n'.format(data['c'])
                     # [TODO] Probably deprecate?
                     output += 'CVr value: **{}**\n\n'.format(data['cv'] if 'cv' in data else data['c'] + 'e')
-                    try:
-                        for i in range(9):
-                            output += '{}. {}\n'.format(str(i + 1), data['d'][i])
-                    except KeyError:
-                        print("Error: not all degrees are present for '{}' in '{}' modifiers list".format(abbreviation, category_name))
-                        raise
+                    for i in range(9):
+                        output += '{}. {}\n'.format(str(i + 1), data['d'][i])
                     output += '\n'
+                    if 'src' in data:
+                        output += 'This modifier is based on the `{}` suffix from [Ithkuil 2011](http://www.ithkuil.net/07_suffixes.htm).\n\n'.format(data['src'].upper())
                     if 'n' in data:
                         output += data['n'] + '\n\n'
-                    if 'src' in data:
-                        output += 'This modifier is based on the `{}` suffix from Ithkuil 2011.\n\n'.format(data['src'].upper())
                     contents += '  * [`{}` {}](#{})\n'.format(abbreviation.upper(), titlecase(data['g']),
                                                              md_linkify(abbreviation + ' ' + data['g']))
                     real_output += output
-                except KeyError:
-                    pass
+                except KeyError as e:
+                    print("WARNING: missing `{}` value on '{}' in '{}' modifiers list".format(e.args[0], abbreviation, category_name))
+                except IndexError:
+                    print("WARNING: not all degrees are present for '{}' in '{}' modifiers list".format(abbreviation, category_name))
     return contents + real_output[:-1] # trim one trailing newline
 
 
